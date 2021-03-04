e562afcedaa3923c6b1980ded0bcc4d9d2476d41
valinor <andresfullstack@gmail.com>
2021-03-04 18:59:01 -0300

    Oden branch creation

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,16 +1,22 @@
-import styles from './Nav.module.css';
-import Link from 'next/link';
+import styles from "./Nav.module.css";
+import Link from "next/link";
 
 export default function Nav() {
-
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li><Link href='/'>Home</Link></li>
-                    <li><Link href='/about'>About</Link></li>
-                </ul>
-            </nav>
-        </div>
-    )
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/practices">Practices 🦍</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
---
+++ pages/practices.js
@@ -0,0 +1,12 @@
+import HeadMetaTags from "../components/Head/Head";
+import Nav from "../components/Nav/Nav";
+
+export default function Practices() {
+  return (
+    <div>
+      <HeadMetaTags title="Practices 🤠" />
+      <Nav />
+      <h1>Entró</h1>
+    </div>
+  );
+}
