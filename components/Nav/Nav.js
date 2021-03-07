88ef662b1781a27502c02e096d1c3f070de73a48
Guishe <gui.bar.jim@gmail.com>
2021-03-06 22:34:26 -0300

    nav bar con animacion lokita

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,16 +1,27 @@
-import styles from './Nav.module.css';
-import Link from 'next/link';
+import styles from "./Nav.module.css";
+import Link from "next/link";
+import gsap from 'gsap';
+import {useRef, useEffect} from 'react';
 
 export default function Nav() {
+    const home = useRef(null);
+    const about = useRef(null);
 
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li><Link href='/'>Home</Link></li>
-                    <li><Link href='/about'>About</Link></li>
-                </ul>
-            </nav>
-        </div>
-    )
+    useEffect(() => {
+        gsap.from(home.current, {y: -50, opacity: 0, duration: 2.2, ease: 'power4.out', delay: 1})
+
+        gsap.from(about.current, {y: -50, opacity: 0, duration: 2.2, ease: 'power4.out', delay: 1, rotation: 360})
+    }, [])
+  return (
+    <nav className={styles.Nav}>
+      <ul>
+        <li ref={home}>
+          <Link  href="/">Home</Link>
+        </li>
+        <li ref={about}>
+          <Link  href="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
