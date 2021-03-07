edbf6078cab96a708f25480356e5e9570734f2d7
Guishe <gui.bar.jim@gmail.com>
2021-03-06 22:45:48 -0300

    keyframe

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import HeadMetaTags from "../components/Head/Head";
 import Nav from "../components/Nav/Nav";
 import styles from "../styles/Home.module.css";
-import gsap from 'gsap';
-import {useRef, useEffect} from 'react';
+
+
 export default function Home() {
+
+
   return (
     <>
       <HeadMetaTags title="Home" />
       <Nav />
       <div className={styles.container}>
-        Container
+        <h1 className={styles.animatedTitle}>Container</h1>
       </div>
     </>
   );
